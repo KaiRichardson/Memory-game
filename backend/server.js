2a1a30ed122d1a30ce86c5f471fdd64c26959be3
Kai R <kairichardson2000@gmail.com>
2019-07-05 15:14:53 -0700

    working local app

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,6 @@ db.once("open", () => console.log("connected to the database"));
 // checks if connection with the database is successful
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-// (optional) only made for logging and
 // bodyParser, parses the request body to be a readable json format
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
