07a4cc6f0d8a4dceaa4a33eb59eee7ff35932b09
Kai R <kairichardson2000@gmail.com>
2019-07-05 16:48:57 -0700

    working local again and routed

--- backend/routes/api-routes.js
+++ backend/routes/api-routes.js
@@ -1,56 +1,56 @@
-const db = require("../models");
+const Data = require("../models");
 
-// this is our get method
-// this method fetches all available data in our database
-app.get("/getData", (req, res) => {
-    db.User.find((err, data) => {
+module.exports = function(app, router) {
+  // this is our get method
+  // this method fetches all available data in our database
+  router.get("/getData", (req, res) => {
+    Data.User.find((err, data) => {
       if (err) return res.json({ success: false, error: err });
       return res.json({ success: true, data: data });
     });
   });
-  
+
   // this is our update method
   // this method overwrites existing data in our database
-  app.post("/updateData", (req, res) => {
+  router.post("/updateData", (req, res) => {
     const { id, update } = req.body;
-    db.User.findByIdAndUpdate(id, update, err => {
+    Data.User.findByIdAndUpdate(id, update, err => {
       if (err) return res.json({ success: false, error: err });
       return res.json({ success: true });
     });
   });
-//   "mongodb://user:password1@ds247637.mlab.com:47637/heroku_l274z5fq";
 
   // this is our delete method
   // this method removes existing data in our database
-  app.delete("/deleteData", (req, res) => {
+  router.delete("/deleteData", (req, res) => {
     const { id } = req.body;
-    db.User.findByIdAndRemove(id, err => {
+    Data.User.findByIdAndRemove(id, err => {
       if (err) return res.send(err);
       return res.json({ success: true });
     });
   });
-  
+
   // this is our create methid
   // this method adds new data in our database
-  app.post("/putData", (req, res) => {
-    let db = new Data();
-  
+  router.post("/putData", (req, res) => {
+    let data = new Data();
+
     const { id, message } = req.body;
-  
+
     if ((!id && id !== 0) || !message) {
       return res.json({
         success: false,
         error: "INVALID INPUTS"
       });
     }
-    db.message = message;
-    db.id = id;
-    db.save(err => {
+    data.message = message;
+    data.id = id;
+    data.save(err => {
       if (err) return res.json({ success: false, error: err });
       return res.json({ success: true });
     });
   });
-  
+
   // append /api for our http requests
-  app.use("/api", app);
-  
+  app.use("/api", router);
+};
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const express = require("express");
 var cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-const Data = require("./data");
 
 const API_PORT = 3001;
 const app = express();
@@ -15,7 +14,7 @@ const dbRoute =
   "mongodb://user:password1@ds247637.mlab.com:47637/heroku_l274z5fq";
 
 // connects our back end code with the database
-mongoose.connect(dbRoute, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || dbRoute, { useNewUrlParser: true });
 
 let db = mongoose.connection;
 
@@ -30,58 +29,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
-// this is our get method
-// this method fetches all available data in our database
-router.get("/getData", (req, res) => {
-  Data.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
-});
-
-// this is our update method
-// this method overwrites existing data in our database
-router.post("/updateData", (req, res) => {
-  const { id, update } = req.body;
-  Data.findByIdAndUpdate(id, update, err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
-
-// this is our delete method
-// this method removes existing data in our database
-router.delete("/deleteData", (req, res) => {
-  const { id } = req.body;
-  Data.findByIdAndRemove(id, err => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
-});
-
-// this is our create methid
-// this method adds new data in our database
-router.post("/putData", (req, res) => {
-  let data = new Data();
-
-  const { id, message } = req.body;
-
-  if ((!id && id !== 0) || !message) {
-    return res.json({
-      success: false,
-      error: "INVALID INPUTS"
-    });
-  }
-  data.message = message;
-  data.id = id;
-  data.save(err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
-
-// append /api for our http requests
-app.use("/api", router);
+// Routes
+require("./routes/api-routes")(app, router);
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
